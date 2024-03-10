dd6dd918a3a3d29f21de0730151ecdd9010728c2
pavithraelavarasi <pavithraelavarasi@gmail.com>
2024-03-10 18:39:13 +0530

    RightAngleTriangle.java

---
+++ level2/RightAngleTriangle.java
@@ -0,0 +1,14 @@
+//16. Write a program in Java to display the pattern like right angle triangle with a number. 
+
+public class RightAngleTriangle {
+ public static void main (String args[])
+ { int num = 10;
+   for (int i =1;i<=10;i++)
+   {
+    for (int j=1;j<=i;j++)
+    System.out.print (j);
+    System.out.println (" ");
+   }
+ }
+}
+ 
