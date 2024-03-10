99786a94dbb0eca92a2e87f76202523a77b13088
pavithraelavarasi <pavithraelavarasi@gmail.com>
2024-03-10 18:39:48 +0530

    ShapePattern24.java

---
+++ level2/ShapePattern24.java
@@ -0,0 +1,13 @@
+//24. Write a java program to generate a following @'s triangle. 
+
+public class ShapePattern24 {
+ public static void main (String args[])
+ { int row =6;
+   for (int i=1;i<=row;i++)
+   {
+    for (int j=1;j<=i;j++)
+    System.out.print ("@");
+    System.out.println (" ");
+   }
+ }
+}
