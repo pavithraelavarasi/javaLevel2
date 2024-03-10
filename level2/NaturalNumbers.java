9ba7066ac9cf95cf6b59135b55f711a4ef4ec0ec
pavithraelavarasi <pavithraelavarasi@gmail.com>
2024-03-10 18:36:41 +0530

    NaturalNumbers.java

---
+++ level2/NaturalNumbers.java
@@ -0,0 +1,13 @@
+//10. Write a program in Java to display the first 10 natural numbers. 
+
+public class NaturalNumbers {
+  public static void main (String args[])
+  { 
+   int num;
+   System.out.println ("The first 10 natural numbers are : ");
+   for (num =1;num<=10;num++)
+   {
+    System.out.println (num);
+   }
+  }
+} 
